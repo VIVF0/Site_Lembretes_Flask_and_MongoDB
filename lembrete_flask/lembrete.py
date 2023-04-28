343e7ce73ca89acc9456cc3f407d4b487134bb30
Vitor Vieira Freire <89537650+VIVF0@users.noreply.github.com>
2023-04-28 15:02:40 -0300

    exibicao_lembretes

--- lembrete_flask/lembrete.py
+++ lembrete_flask/lembrete.py
@@ -31,22 +31,26 @@ def banco_lembretes(db,id_usuario,lembrete,hora_criacao,dia_criacao,hora_ativa,d
                 'Hora_ativa':hora_ativa,
                 'Dia_ativa':dia_ativa,
                 'Hora_criacao':hora_criacao,
-                'Dia_criacao':dia_criacao
+                'Dia_criacao':dia_criacao,
+                'Status':'aberto'
             }
         )
+        return True
+    except:
+        return False
+
+def fecha_lembrete(db,id_lembrete):
+    banco=db['lembrete']
+    try: 
+        banco.update_one({'_id':id_lembrete},{'Status':'fechado'})
         return True
     except:
         return False
     
 def busca_lembrete(db,id_usuario):
     banco=db['lembrete']
-    lembrete={}
     resultado=banco.find({"Id":id_usuario})
-    i=0
-    for item in resultado:
-        lembrete[i][item.key]=item.value
-        i+=1
-    return lembrete
+    return resultado
 
 def exite_usuario(db,busca,alvo):
     banco=db['usuario']
@@ -70,16 +74,20 @@ def hora_min():
 
 def autentica_senha(EmailEntrada,senhaEntrada,db):
     banco=db['usuario']
-    resultado=banco.find_one({EmailEntrada:criptografar(senhaEntrada)})
+    resultado=banco.find_one({'Email':EmailEntrada,'Senha':criptografar(senhaEntrada)})
     if resultado!=None:
         return True
     else:
         return False
+    
+def busca_usuario(db,email):
+    banco=db['usuario']
+    resultado=banco.find_one({'Email':email})
+    return resultado
+    
 
 client=MongoClient('localhost',27017)
 db=client.lembrete_mongodb
-#busca_lembrete(db,'6414cc87c1cae14e563f0401')
-
 
 app = Flask(__name__)
 app.secret_key = 'lembrete'
@@ -95,9 +103,9 @@ def login():
 @app.post('/login')
 def autenticar():
     if autentica_senha(request.form['email'],request.form['senha'],db):
-        email=request.form['email']
-        session['usuario'] = email
-        flash(email + ' logado com sucesso!')
+        usuario=busca_usuario(db,request.form['email'])
+        session['usuario'] = str(usuario['_id'])
+        flash(request.form['email'] + ' logado com sucesso!')
         return redirect(url_for('lembrete'))
     else:
         flash('Usuário não logado.')
@@ -115,22 +123,20 @@ def cadastro_usuario():
 
 @app.route('/cadastro_usuario', methods=['POST',])
 def cadastro_novo_usuario():
-    try:
-        if exite_usuario(db,'email',request.form['email']):
-            cadastro=banco_usuario(db,request.form['email'],request.form['nome'],request.form['senha'])
-            if cadastro:
-                session['usuario'] = request.form['email']
-                flash(request.form['email'] + ' criado com suesso!')
-                return redirect((url_for('cadastro_usuario')))
-            else:
-                flash('Não foi possivel cadastrar o usuario')
-                return redirect((url_for('cadastro_usuario')))
+    email = request.form['email']
+    nome = request.form['nome']
+    senha = request.form['senha']
+    if exite_usuario(db,'email', email):
+        if banco_usuario(db, email, nome, senha):
+            usuario=busca_usuario(db, email)
+            session['usuario'] = usuario['_id']
+            flash(usuario['Nome'] + ' criado com sucesso!')
         else:
-            flash('Não foi possivel cadastrar o usuario')
-            return redirect((url_for('cadastro_usuario')))
-    except:
-        flash('Não foi possivel cadastrar o usuario')
-        return redirect((url_for('cadastro_usuario')))
+            flash('Não foi possível cadastrar o usuário')
+    else:
+        flash('Não foi possível cadastrar o usuário')
+    return redirect(url_for('cadastro_usuario'))
+
 
 @app.route('/cadastro_lembrete') 
 def cadastro_lembrete():
@@ -142,13 +148,16 @@ def cad_lembrete():
         return redirect(url_for('login'))
     else:
         banco_lembretes(db,session['usuario'],request.form['lembrete'],hora_min(),dia_mes_ano(),request.form['hora'],request.form['dia'])
-        return redirect((url_for('lembrete')))
+        return redirect(url_for('lembrete'))
     
 @app.route('/lembrete')
 def lembrete():
-    #if 'usuario' not in session or session['usuario'] == None:
-    #    return redirect(url_for('login', proxima=url_for('lembrete')))
-    usuario=busca_usuario(db,'usuario','Email','vitor')
-    #return render_template('lembrete.html',lembrete=busca_lembrete(db,usuario[]), titulo='Lembretes',id_usuario=session['usuario'])
-    
+    lembrete=busca_lembrete(db,session['usuario'])
+    return render_template('lembrete.html',titulo="Lembretes",lembrete=lembrete)
+
+@app.route('/fecha_lembrete', methods=['POST',])
+def status_lembrete():
+    fecha_lembrete(db,request.form['item_id'])
+    return redirect(url_for('lembrete'))
+
 app.run(debug=True)
